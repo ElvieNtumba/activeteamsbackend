7951722f9a877a73d2470f0f838a0aac4763fab2
Keren <kerenbotombe125@gmail.com>
2025-08-07 21:08:14 +0200

    Fixed some folders and imports

--- auth/models.py
+++ auth/models.py
@@ -11,9 +11,22 @@ class UserCreate(BaseModel):
     gender: str
     password: str
     
-    
-    
-
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Event(BaseModel):
+    eventType: str
+    service_name: str
+    date: str 
+    location: str
+    total_attendance: int = 0
+    attendees: list[dict] = []
+
+class CheckIn(BaseModel):
+    event_id: str
+    name: str
+
+class UncaptureRequest(BaseModel):
+    event_id: str
+    name: str
--- db/mongo.py
+++
@@ -1,9 +0,0 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGODB_URI = os.getenv("MONGODB_URI")
-client = AsyncIOMotorClient(MONGODB_URI)
-db = client["active-teams-db"]  # ← change to your actual DB name
